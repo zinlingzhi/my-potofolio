394950c509814fe8c6e7be3788e98fc24be5b635
zinlingzhi <zinlingzhi@gmail.com>
2022-04-14 23:20:04 +0800

    Update the commit

---
+++ src/pages/about.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'gatsby';
+// Gatsby About Page
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Me</title>
+      <h1>About Me</h1>
+      <Link to="/">Back to Home</Link>
+      <p>Hi, there! I'm proud creator of this site, which I built with Gatsby.</p>
+    </main>
+  )
+}
+
+export default AboutPage
