2b93001ad72c9be1e5c62e8f9bc316ccd714ac57
zinlingzhi <zinlingzhi@gmail.com>
2022-04-28 00:28:38 +0800

    Update the blogs path and context of each blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import Layout from '../components/layout';
-import { graphql } from 'gatsby';
+import Layout from '../../components/layout';
+import { Link, graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPage = ({data}) => {
@@ -9,11 +9,13 @@ const BlogPage = ({data}) => {
     {
       data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+              <Link to={`/blog/${node.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
           <p>Posted: {node.frontmatter.date}</p>
-          <MDXRenderer>
-            {node.body}
-          </MDXRenderer>
+
         </article>
       ))
     }
@@ -30,7 +32,7 @@ export const query = graphql`
           date(formatString: "MMMM DD, YYYY")
         }
         id
-        body
+        slug
       }
     }
   }
