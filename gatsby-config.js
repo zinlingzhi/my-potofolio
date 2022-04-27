49e69fcbb48b3c28cadccb93b75258435ff60e4a
zinlingzhi <zinlingzhi@gmail.com>
2022-04-28 00:10:59 +0800

    Update the Mdx Plugin to show the blogs using mdx files. - Create blog mdx files in blog folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,18 @@
 module.exports = {
   siteMetadata: {
+    title: "my potofolio",
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
+    "gatsby-plugin-mdx"
   ],
 }
---
+++ src/pages/blog.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+
+const BlogPage = ({data}) => {
+  return (
+    <Layout pagetTitle="My Blog Posts">
+    {
+      data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>
+            {node.body}
+          </MDXRenderer>
+        </article>
+      ))
+    }
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+        }
+        id
+        body
+      }
+    }
+  }
+`
+
+export default BlogPage
