9ab1dbe75473da26f7f31ed08063e28d735fef84
zinlingzhi <zinlingzhi@gmail.com>
2022-05-09 05:01:00 +0800

    Update the about

--- src/components/about.js
+++ src/components/about.js
@@ -137,7 +137,7 @@ const About = () => {
           <div>
             <p>
               Hello! My name is LingZhi and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
+              interest in web development started back in 2011 when I decided to try editing custom
               Tumblr themes — turns out hacking together a custom reblog button taught me a lot
               about HTML &amp; CSS!
             </p>
